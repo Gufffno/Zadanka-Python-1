689467b0e749a3c43b94b1deace456a326c28a46
Gufffno <62694754+Gufffno@users.noreply.github.com>
2020-04-23 15:00:26 +0200

    Create zadanie13.py

---
+++ zadanie13.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+slownik = {'Pelikan': 'Jaś Święs', 'Chudy': 'Bartosz Obudziński', 'Agent': 'Marek Krzymowksi', 'Góral': 'Jakub Rydzewski', 'Krecik': 'Piotr Jarema', 'Myketo': 'Mikołaj Pięcek', 'Cringe': 'Darek Letkiewicz', 'Jednoręki Bandyta': 'Juliusz Makulec', 'El prezidento': 'Dariusz Oleśniewicz', 'Struś': 'Klaudia Rowińska' }
+print(slownik['Pelikan'])
+print(slownik['Chudy'])
+print(slownik['Agent'])
+print(slownik['Góral'])
+print(slownik['Krecik'])
+print(slownik['Myketo'])
+print(slownik['Cringe'])
+print(slownik['Jednoręki Bandyta'])
+print(slownik['El prezidento'])
+print(slownik['Struś'])
