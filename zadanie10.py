209456b1a5be81dbedc728350ccba0fa4e247975
Gufffno <62694754+Gufffno@users.noreply.github.com>
2020-04-23 15:00:06 +0200

    Create zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+lista = ["Deadpool", "Limitless", "Las Vegas Parano", "Świat według Kiepskich", "That's the 70's show", "SpongeBobo SquarePants The movie", "Project X", "Fanatyk", "Kung Fu Panda", "Monster's Inc.", "Cars"]
+print(lista)
+
+lista.sort()
+print(lista)
